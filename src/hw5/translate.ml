24dc9d466116baf36141bf292c2afad6ee797bac
Jason Kim <actcrazy@gmail.com>
2022-05-03 00:12:21 +0900

    hw5: test5 까지 완료

--- src/hw5/translate.ml
+++ src/hw5/translate.ml
@@ -12,10 +12,23 @@ module Translator = struct
   let rec trans : K.program -> Sm5.command = function
     | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
+    | K.SUB (e1, e2) -> trans e1 @ trans e2 @ [Sm5.SUB]
+    | K.MUL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.MUL]
+    | K.LESS (e1, e2) -> trans e1 @ trans e2 @ [Sm5.LESS]
     | K.LETV (x, e1, e2) -> begin
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
     end
-    | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
+    | K.LETF (f, x, e1, e2) -> begin
+      [Sm5.PUSH (Sm5.Fn(x, [Sm5.BIND f] @ (trans e1))); Sm5.BIND f] @ trans e2 @ [Sm5.UNBIND; Sm5.POP]
+    end
+    | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE;]
+    | K.VAR x -> [Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
+    | K.WRITE e -> trans e @ [Sm5.PUT]
+    | K.SEQ (e1, e2) -> trans e1 @ trans e2
+    | K.ASSIGN (x, e) -> trans e @ [Sm5.PUSH (Sm5.Id x); Sm5.STORE]
+    | K.CALLV (f, arg_exp) -> [Sm5.PUSH (Sm5.Id f); Sm5.PUSH (Sm5.Id f)] @ trans arg_exp @ [Sm5.MALLOC; Sm5.CALL]
+    | K.CALLR (f, arg_var) -> [Sm5.PUSH (Sm5.Id f); Sm5.PUSH (Sm5.Id f)] @ trans (K.VAR arg_var) @ [Sm5.PUSH (Sm5.Id arg_var); Sm5.CALL]
+    | K.IF (e_cond, e_true, e_false) -> trans e_cond @ [Sm5.JTR(trans e_true, trans e_false)]
     | _ -> failwith "Unimplemented"
 end
